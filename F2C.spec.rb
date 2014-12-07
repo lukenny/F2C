f7c766c854ea03146b085f3cb68232dfd0e98a4d
Kenny Lu <klu@avvo.com>
2014-12-06 16:03:32 -0800

    updated spec.rb

--- F2C.spec.rb
+++ F2C.spec.rb
@@ -4,12 +4,12 @@ require 'F2C.rb'
 
 describe Convert do
 	
-	it "converts Fahrenheit to Celsius" do
+	it "Fahrenheit to Celsius" do
       x = Convert.F2C(83)
-      expect(x).to eq "83 Fahrenheit is 28 Celsius"
+      expect(x).to eq  "83 Fahrenheit is 28 Celsius"
     end
 
-    it "converts Celsius to Fahrenheit" do
+    it "Celsius to Fahrenheit" do
       x = Convert.C2F(18)
       expect(x).to eq "18 Celsius is 64 Fahrenheit"
     end
